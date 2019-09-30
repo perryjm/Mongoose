8faa8553bf1cd4e0996e407e63c6dc99d53dbc45
Ben Thornhill <benthornhill@bens-mbp.ad.uvu.edu>
2019-09-30 17:05:52 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+//import mongoose from 'mongoose';
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://bthorn19:Skywalker3@cluster0-jdspq.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true });
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    var movieSchema = new mongoose.Schema({
+        title: String,
+        year: String
+      });
+
+    var personSchema = new mongoose.Schema({
+        firstName: String,
+        lastName: String,
+        birthDate: Date
+    });
+
+    var actorSchema = new mongoose.Schema({
+        
+    });
+
+    var directorSchema = new mongoose.Schema({
+        
+    });
+
+      
+    var Movie = mongoose.model('Movie', movieSchema);
+    
+    var jurassicPark = new Movie({ title: 'Jurassic Park', year: '1995' });
+    
+    // jurassicPark.save(function (err, jurassicPark) {
+    //     if (err) return console.error(err);
+    //   });
+    jurassicPark.save().then(() => {
+        console.log("success");
+    })
+
+});
+
